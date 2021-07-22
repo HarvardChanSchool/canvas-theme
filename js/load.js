cb45b79b129de9125be0de9a28c26c10da9f69e5
guillaumemolter <gmolter@hsph.harvard.edu>
2021-07-22 16:59:00 -0400

    Loading CSS via JS CAN-23

--- js/load.js
+++ js/load.js
@@ -17,14 +17,33 @@ function hsphloadScript(src) {
 	document.getElementsByTagName('head')[0].appendChild(s);
 }
 
+ /**
+ * Stylesheet loader function.
+ * 
+ * @param string src Stylesheet source URL to be loaded.
+ */ 
+  function hsphloadStylesheet(src) {
+	var s = document.createElement('link');
+	s.type = 'text/css';
+	s.rel = 'stylesheet';
+	s.src = src;
+	document.getElementsByTagName('head')[0].appendChild(s);
+}
+
 if( window.location.href.indexOf( 'harvard.test.instructure.com' ) > -1 ) {
+	// Load dev JS
 	hsphloadScript( 'https://content.sph.harvard.edu/it/canvas-theme/dev/js/manifest.js' );
 	hsphloadScript( 'https://content.sph.harvard.edu/it/canvas-theme/dev/js/vendor.js' );
 	hsphloadScript( 'https://content.sph.harvard.edu/it/canvas-theme/dev/js/app.js' );
+	// Load dev CSS
+	hsphloadStylesheet( 'https://content.sph.harvard.edu/it/canvas-theme/dev/css/app.css' );
 } else {
+	// Load prod JS
 	hsphloadScript( 'https://content.sph.harvard.edu/it/canvas-theme/prod/js/manifest.js' );
 	hsphloadScript( 'https://content.sph.harvard.edu/it/canvas-theme/prod/js/vendor.js' );
-	hsphloadScript( 'https://content.sph.harvard.edu/it/canvas-theme/prod/js/app.js' );
+	hsphloadStylesheet( 'https://content.sph.harvard.edu/it/canvas-theme/prod/js/app.css' );
+	// Load prod CSS
+	hsphloadStylesheet( 'https://content.sph.harvard.edu/it/canvas-theme/dev/prod/app.css' );
 }
 
 // add the akltura script to the head of our document.
